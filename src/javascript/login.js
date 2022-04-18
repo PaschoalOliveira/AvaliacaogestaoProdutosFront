f3b1f57fc5e1644fbe8cf9e7a45795c4a2b49f06
unknown <paschoaloliveira@hotmail.com>
2022-04-18 17:55:37 -0300

    commit inicial

---
+++ src/javascript/login.js
@@ -0,0 +1,17 @@
+
+function validarLogin(){
+    let login = document.getElementById("login").value;
+    let senha = document.getElementById("senha").value;
+
+    var myHeaders = new Headers();
+    var myInit = { method: 'GET',
+               headers: myHeaders,
+               mode: 'cors',
+               cache: 'default' };
+
+    fetch('http://127.0.0.1:8080/usuairos/validaLogin?login=' + login + '&senha=' + senha,myInit)
+    .then(function(response) {
+        console.log(response);
+      })
+
+}
